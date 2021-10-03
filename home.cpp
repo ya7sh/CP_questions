05a89a914ad6ae584cb2952837fe7cb63ff55ef9
Paras Lohia <paraslohia126@gmail.com>
2021-10-03 23:32:13 +0530

    hello05

--- home.cpp
+++ home.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int main() 
 {
+	int a=0;
+	if(a==0)
+	{
+
 	
 	cout<<"The Following two files conatin two ques";
 	cout<<"I made a new change to this file";
 	cout<<"Simple C++ code";
 	cout<<"i am change";
 	return 0;
+	}
 }
